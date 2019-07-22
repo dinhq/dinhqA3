4dc953f34a3b335a46d59920f0de5ebacc0160d5
dinhq <52496793+dinhq@users.noreply.github.com>
2019-07-22 16:37:34 -0400

    Completed project

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,32 @@
+import { Book } from '../../app/book';
+
+export const MyBooks: Book[] = [
+    {
+        name: 'Harry Potter',
+        author: 'J.K Rowling',
+        genre: 'Fantasy',
+        publishedYear: '1997',
+        picture: 'assets/images/harrypotter-1.jpg'
+    },
+    {
+        name: 'Game of Thrones',
+        author: 'George R. R. Martin',
+        genre: 'Epic Fantasy',
+        publishedYear: '1996',
+        picture: 'assets/images/game-of-thrones.jpg'
+    },
+    {
+        name: 'Sherlock Homes',
+        author: 'Sir Arthur Conan Doyle',
+        genre: 'Mystery, Detective Fiction',
+        publishedYear: '2011',
+        picture: 'assets/images/the-complete-sherlock-homes.jpg'
+    },
+    {
+        name: 'The Witcher',
+        author: 'Andrzej Sapkowski',
+        genre: 'Fantasy',
+        publishedYear: '1993',
+        picture: 'assets/images/the-last-wish.jpg'
+    }
+];
